c5d595f61b1cb8cc7ba1f1f7bb2bf1333508af6d
Komal Raza <komal123088@gmail.com>
2026-04-16 21:56:46 +0500

    fix: add env.js for production

---
+++ env.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+module.exports = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+};
